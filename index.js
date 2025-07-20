dd9c0e02bb4333de41a510b2d92cffb5cf434830
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-20 11:38:03 +0545

    backend learning using express creating port

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hi i am learning backend")
+
+import dotenv from 'dotenv'
+dotenv.config()
+import express from "express"
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send("Hello world!")
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('You are currently on twitter')
+})
+
+
+app.get('/login', (req, res) => {
+    res.send(`<h1>Hi from h1</h1>`)
+})
+
+app.get('/basantkhatri', (req, res) => {
+    res.send('My page')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`App listening on ${port}`)
+})
