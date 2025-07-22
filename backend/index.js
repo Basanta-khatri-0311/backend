08df3816dbf33255a5e77a9fc13c251796c9df08
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2025-07-22 12:01:39 +0545

    rying to fetch datas

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,22 @@
+// import express from "express";
+
+// const app = express();
+
+// const PORT = process.env.PORT || 3000;
+
+// app.get("/api/joke", (req, res) => {
+//   const jokes = [
+//     { id: 1, title: " A joke 1", content: "This is a joke 1 " },
+//     { id: 2, title: " A joke 2", content: "This is a joke 2 " },
+//     { id: 3, title: " A joke 3", content: "This is a joke 3 " },
+//   ];
+//   res.send(jokes);
+// });
+
+// app.listen(PORT, () =>
+//   console.log(`Server is runnning on http://localhost:${PORT}`)
+// );
+
 import express from "express";
 
 const app = express();
@@ -13,6 +32,20 @@ app.get("/api/joke", (req, res) => {
   res.send(jokes);
 });
 
+app.get("/api/about", (req, res) => {
+  const user = { name: "Basanta Khatri", gender: "Male", age: 23 };
+  res.send(user);
+});
+
+app.get("/api/contact", (req, res) => {
+  res.send(
+    "Please visit https://basanta-khatri.netlify.app for more information"
+  );
+});
+
+app.get("/api/quote", (req, res) => {
+  res.send("This a from quote");
+});
 
 app.listen(PORT, () =>
   console.log(`Server is runnning on http://localhost:${PORT}`)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,73 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { response } from "express";
 
 const App = () => {
   const [jokes, setJokes] = useState([]);
+  const [about, setAbout] = useState();
+  const [quote, setQuote] = useState();
+  const [contact, setContact] = useState();
 
-  useEffect(() => {
-    
+  const fetchJokes = () => {
     axios
       .get("/api/joke")
       .then((response) => setJokes(response.data))
       .catch((error) => console.log(error));
-  }, []);
+  };
+  const fetchQuotes = () => {
+    axios
+      .get("/api/quote")
+      .then((res) => setQuote(res.data))
+      .catch((e) => console.log(e));
+  };
 
+  const fetchAbout = () => {
+    axios
+      .get("/api/about")
+      .then((res) => setAbout(res.data))
+      .catch((e) => console.log(e));
+  };
+
+  const fetchContact = () => {
+    axios
+      .get("/api/contact")
+      .then((res) => setContact(res.data))
+      .catch((e) => console.log(e));
+  };
   return (
     <>
-      <p>jokes: {jokes.length}</p>
-      {jokes.map((joke) => (
-        <div key={joke.id}>
-          <h3>{joke.title}</h3>
-          <p>{joke.content}</p>
-        </div>
-      ))}
+      <div>
+        <button onClick={fetchJokes}>Jokes</button>
+        <button onClick={fetchAbout}>About</button>
+        <button onClick={fetchContact}>Contact</button>
+        <button onClick={fetchQuotes}>Quotes</button>
+      </div>
+      <div>
+        {jokes.map((joke) => (
+          <div key={joke.id}>
+            <p>{joke.title}</p>
+            <p>{joke.content}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h2>Here we can find my about</h2>
+        {about &&
+          Object.entries(about).map(([key, value]) => (
+            <p key={key}>
+              <strong>{key}:</strong> {value}
+            </p>
+          ))}
+      </div>
+
+      <div>
+        <h2>Here is a quote</h2>
+        <p>{quote}</p>
+      </div>
+
+      <div>
+        <h2>Here is a contact</h2>
+        <p>{contact}</p>
+      </div>
     </>
   );
 };
